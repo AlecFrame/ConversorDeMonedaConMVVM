237e23426c61f98b63cdc227096baf1f014a4776
AlexFrame <walteravertacnik@gmail.com>
2026-03-27 17:44:32 -0300

    Primer diseño

---
+++ app/src/main/java/com/vertacnik/conversor_moneda/MainViewModel.java
@@ -0,0 +1,14 @@
+package com.vertacnik.conversor_moneda;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainViewModel extends AndroidViewModel {
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+}
