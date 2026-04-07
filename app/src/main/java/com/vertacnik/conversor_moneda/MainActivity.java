853a088b104aec52371d85a7211e1d49f4d239e6
AlexFrame <walteravertacnik@gmail.com>
2026-04-07 17:50:15 -0300

    Cambios MVVM
    MVVM, olvide subirlo

--- app/src/main/java/com/vertacnik/conversor_moneda/MainActivity.java
+++ app/src/main/java/com/vertacnik/conversor_moneda/MainActivity.java
@@ -3,16 +3,9 @@ package com.vertacnik.conversor_moneda;
 import static android.app.ProgressDialog.show;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.vertacnik.conversor_moneda.databinding.ActivityMainBinding;
@@ -21,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MainViewModel viewModel;
+    private String valor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,38 +23,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainViewModel.class);
 
-        viewModel.getUnidadDeCambio().observe(this, unidadDeCambio -> {
-
+        viewModel.getUnidadDeCambio().observe(this, s -> {});
+        viewModel.getValorString().observe(this, s -> {
+            valor = s;
+        });
+        viewModel.getStringEuro().observe(this, s -> {
+            binding.etEuro.setText(s);
+        });
+        viewModel.getStringDolar().observe(this, s -> {
+            binding.etDolar.setText(s);
         });
 
         // Al seleccionar el radio button de Dolar se activan y desactivan los campos correspondientes y se setea el texto del valor de conversión
         binding.rbDolar.setOnClickListener(v -> {
+            viewModel.setUnidadDeCambio(binding.rbEuro.isChecked());
             binding.etDolar.setEnabled(false);
             binding.etDolar.setText("");
             binding.etEuro.setEnabled(true);
             binding.etEuro.setText("");
             binding.etUnidad.setEnabled(true);
-            binding.etUnidad.setText(
-                    String .valueOf(1/viewModel.getUnidadDeCambio().getValue())
-            );
+            binding.etUnidad.setText(valor);
         });
 
         // Al seleccionar el radio button de Euro se activan y desactivan los campos correspondientes y se setea el texto del valor de conversión
         binding.rbEuro.setOnClickListener(v -> {
+            viewModel.setUnidadDeCambio(binding.rbEuro.isChecked());
             binding.etDolar.setEnabled(true);
             binding.etDolar.setText("");
             binding.etEuro.setEnabled(false);
             binding.etEuro.setText("");
             binding.etUnidad.setEnabled(true);
-            binding.etUnidad.setText(
-                    String .valueOf(viewModel.getUnidadDeCambio().getValue())
-            );
+            binding.etUnidad.setText(valor);
         });
 
         // Al apretar el botón convertir llama al método del model convertir y les pasamos los componentes que requiere
         binding.btConvertir.setOnClickListener(v -> {
             viewModel.convertir(
-                    binding.rbDolar, binding.etEuro, binding.etDolar, binding.rbEuro
+                    binding.rbDolar.isChecked(),
+                    binding.rbEuro.isChecked(),
+                    binding.etEuro.getText().toString(),
+                    binding.etDolar.getText().toString()
             );
         });
 
@@ -69,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
             viewModel.controlDeValorDeConversion(binding.etUnidad);
 
             // guarda en el viewModel el valor
-            viewModel.setUnidadDeCambio(Double.parseDouble(binding.etUnidad.getText().toString()), binding.rbEuro.isChecked());
+            viewModel.cambiarUnidadDeCambio(binding.etUnidad.getText().toString(), binding.rbEuro.isChecked());
         });
     }
 }
--- app/src/main/java/com/vertacnik/conversor_moneda/MainViewModel.java
+++ app/src/main/java/com/vertacnik/conversor_moneda/MainViewModel.java
@@ -10,16 +10,38 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import java.util.List;
-
 public class MainViewModel extends AndroidViewModel {
 
     // Nosotros hicimos la lógica de conversion en base al valor de cambio entre dólar a euro
     private MutableLiveData<Double> unidadDeCambio;
+    private MutableLiveData<String> valorString;
+    private MutableLiveData<String> stringEuro;
+    private MutableLiveData<String> stringDolar;
     public MainViewModel(@NonNull Application application) {
         super(application);
     }
 
+    public LiveData<String> getValorString() {
+        if (valorString==null) {
+            valorString = new MutableLiveData<>();
+            valorString.setValue("0.87");
+        }
+        return valorString;
+    }
+    public LiveData<String> getStringEuro() {
+        if (stringEuro==null) {
+            stringEuro = new MutableLiveData<>();
+            stringEuro.setValue("");
+        }
+        return stringEuro;
+    }
+    public LiveData<String> getStringDolar() {
+        if (stringDolar==null) {
+            stringDolar = new MutableLiveData<>();
+            stringDolar.setValue("");
+        }
+        return stringDolar;
+    }
     public LiveData<Double> getUnidadDeCambio() {
         if (unidadDeCambio==null) {
             unidadDeCambio = new MutableLiveData<>();
@@ -28,17 +50,37 @@ public class MainViewModel extends AndroidViewModel {
         return unidadDeCambio;
     }
 
+
     // Antes de cambiar el valor de conversión por el de pasado por paramatro, verificamos que radio button está seleccionado para aplicar la lógica de conversion según corresponda
-    public void setUnidadDeCambio(double unidadNueva, boolean isDolar) {
+    public void cambiarUnidadDeCambio(String unidadNueva, boolean isDolar) {
+        double valor;
+
+        try {
+            valor = Double.parseDouble(unidadNueva);
+        } catch (NumberFormatException e) {
+            Toast.makeText(getApplication(), "El campo de Unidad de conversión posee debe ser un número", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         if (isDolar) {
-            unidadDeCambio.setValue(unidadNueva);
+            unidadDeCambio.setValue(valor);
+            valorString.setValue(String.valueOf(valor));
         }else {
-            unidadDeCambio.setValue(1/unidadNueva);
+            unidadDeCambio.setValue(1/valor);
+            valorString.setValue(String.valueOf(1/valor));
         }
     }
 
-    // ve que radio button está seleccionado para convertir de dólar a euro o euro a dólar
-    public void convertir(RadioButton rbDolar, EditText etEuro, EditText etDolar, RadioButton rbEuro) {
+    public void setUnidadDeCambio(boolean euro) {
+        if (euro) {
+            valorString.setValue(unidadDeCambio.getValue().toString());
+        }else {
+            valorString.setValue(String.valueOf(1/unidadDeCambio.getValue()));
+        }
+    }
+
+    // Recuerdos de errores pasados
+    private void convertir(RadioButton rbDolar, EditText etEuro, EditText etDolar, RadioButton rbEuro) {
         if (rbDolar.isChecked()) {
             // Verificamos que el campo de euro no esté vacío
             if (etEuro.getText().toString().isEmpty()) {
@@ -67,6 +109,33 @@ public class MainViewModel extends AndroidViewModel {
         }else {
             Toast.makeText(getApplication(), "No hay ningún opción seleccionado", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void convertir(boolean dolar, boolean euro, String stringEuro, String stringDolar) {
+        if (dolar) {
+            convertirDolar(stringEuro);
+        }else if (euro) {
+            convertirEuro(stringDolar);
+        }else {
+            Toast.makeText(getApplication(), "No se ha seleccionado ningúna opción a que convertir", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void convertirDolar(String etEuro) {
+        // Verificamos que el campo de euro no esté vacío
+        if (etEuro.isEmpty()) {
+            Toast.makeText(getApplication(), "El campo de euro está vacío", Toast.LENGTH_LONG).show();
+        }
+        // Seteamos el valor de conversion en el otro campo
+        stringDolar.setValue(String.valueOf(Double.parseDouble(etEuro) * 1/getUnidadDeCambio().getValue() ));
+    }
+
+    private void convertirEuro(String etDolar) {
+        // Verificamos que el campo de dólar no esté vacío
+        if (etDolar.isEmpty()) {
+            Toast.makeText(getApplication(), "El campo de dólar está vacío", Toast.LENGTH_LONG).show();
+        }
+        // Seteamos el valor de conversion en el otro campo
+        stringEuro.setValue(String.valueOf(Double.parseDouble(etDolar) * getUnidadDeCambio().getValue() ));
     }
 
     public void controlDeValorDeConversion(EditText etUnidad) {
